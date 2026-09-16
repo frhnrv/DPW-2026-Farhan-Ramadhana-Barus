385849e6ead8293055ae1fe9533cd7257fcfe903
frhnrv <farhanramadhanabarus@gmail.com>
2026-09-16 15:34:02 +0700

    Jobsheet 5 - hamburger menu

---
+++ Jobsheet 5/assets/js/app.js
@@ -0,0 +1,10 @@
+// Hamburger Menu
+function initNavToggle() {
+    const toggleBtn = document.getElementById("nav-toggle-btn");
+    const nav = document.querySelector("header nav");
+    if (!toggleBtn || !nav) return;
+
+    toggleBtn.addEventListener("click", function() {
+        nav.classList.toggle("nav-open");
+    });
+}
